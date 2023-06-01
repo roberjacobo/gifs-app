e06f7894fae1d04f573d0ebae06c463865cc3cd9
jose jacobo <jose.jacobo@globant.com>
2023-06-01 00:02:55 -0600

    new environmental variables added

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -5,9 +5,15 @@ import { CommonModule } from "@angular/common";
 import { HomePageComponent } from "./pages/home/home-page.component";
 import { SearchBoxComponent } from "../gifs/components/search-box/search-box.component";
 import { CardListComponent } from "../gifs/components/card-list/card-list.component";
+import { CardComponent } from "./components/card/card.component";
 
 @NgModule({
-  declarations: [HomePageComponent, SearchBoxComponent, CardListComponent],
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent,
+    CardComponent,
+  ],
   imports: [CommonModule],
   exports: [HomePageComponent],
 })
