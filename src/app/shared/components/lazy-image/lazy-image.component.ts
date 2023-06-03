33938329876824b90cb43dcfb2fc8f484d9d1587
jose jacobo <jose.jacobo@globant.com>
2023-06-03 16:07:33 -0600

    build:github implemented

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit } from "@angular/core";
 @Component({
   selector: "lazy-image",
   templateUrl: "./lazy-image.component.html",
+  styleUrls: ["./lazy-image.component.css"],
 })
 export class LazyImageComponent implements OnInit {
   @Input()
